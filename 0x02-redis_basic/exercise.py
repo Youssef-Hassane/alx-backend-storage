d2281499094b032bba961747bd457eea6d441129
youssef-hassane <youssef_hassane@icloud.com>
2024-08-15 02:43:51 +0300

    DONE

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,35 +1,19 @@
 #!/usr/bin/env python3
-"""
-Redis module
-"""
-import sys
-from functools import wraps
-from typing import Union, Optional, Callable
-from uuid import uuid4
+"""doc doc module"""
 
 import redis
-
-UnionOfTypes = Union[str, bytes, int, float]
+import uuid
+from typing import Union, Callable, Optional
+from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """
-    a system to count how many
-    times methods of the Cache class are called.
-    :param method:
-    :return:
-    """
-    key = method.__qualname__
+    """doc doc class"""
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """
-        Wrap
-        :param self:
-        :param args:
-        :param kwargs:
-        :return:
-        """
+        """doc doc class"""
+        key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
 
@@ -37,71 +21,67 @@ def count_calls(method: Callable) -> Callable:
 
 
 def call_history(method: Callable) -> Callable:
-    """
-    add its input parameters to one list
-    in redis, and store its output into another list.
-    :param method:
-    :return:
-    """
-    key = method.__qualname__
-    i = "".join([key, ":inputs"])
-    o = "".join([key, ":outputs"])
+    """doc doc class"""
+    inkey = method.__qualname__ + ":inputs"
+    outkey = method.__qualname__ + ":outputs"
 
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """ Wrapp """
-        self._redis.rpush(i, str(args))
+        """doc doc class"""
+        self._redis.rpush(inkey, str(args))
         res = method(self, *args, **kwargs)
-        self._redis.rpush(o, str(res))
+        self._redis.rpush(outkey, str(res))
         return res
 
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """doc doc class"""
+    input_key = "{}:inputs".format(method.__qualname__)
+    output_key = "{}:outputs".format(method.__qualname__)
+
+    inputs = method.__self__._redis.lrange(input_key, 0, -1)
+    outputs = method.__self__._redis.lrange(output_key, 0, -1)
+
+    print("{} was called {} times:".format(method.__qualname__, len(inputs)))
+    for inp, out in zip(inputs, outputs):
+        print(
+            "{}(*{}) -> {}".format(
+                method.__qualname__, inp.decode("utf-8"), out.decode("utf-8")
+            )
+        )
+
+
 class Cache:
-    """
-    Cache redis class
-    """
+    """doc doc class"""
 
     def __init__(self):
-        """
-        constructor of the redis model
-        """
+        """doc doc method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     @call_history
-    def store(self, data: UnionOfTypes) -> str:
-        """
-        generate a random key (e.g. using uuid),
-         store the input data in Redis using the
-          random key and return the key.
-        :param data:
-        :return:
-        """
-        key = str(uuid4())
-        self._redis.mset({key: data})
-        return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """doc doc method"""
+        keyx = str(uuid.uuid4())
+        self._redis.set(keyx, data)
+        return keyx
 
-    def get(self, key: str, fn: Optional[Callable] = None) \
-            -> UnionOfTypes:
-        """
-        convert the data back
-        to the desired format
-        :param key:
-        :param fn:
-        :return:
-        """
+    def get(
+        self, key: str, fn: Optional[Callable] = None
+    ) -> Union[str, bytes, int, float]:
+        """doc doc method"""
+        value = self._redis.get(key)
         if fn:
-            return fn(self._redis.get(key))
-        data = self._redis.get(key)
-        return data
+            value = fn(value)
+        return value
 
-    def get_int(self: bytes) -> int:
-        """get a number"""
-        return int.from_bytes(self, sys.byteorder)
+    def get_str(self, key: str) -> str:
+        """doc doc method"""
+        return self.get(key, fn=str)
 
-    def get_str(self: bytes) -> str:
-        """get a string"""
-        return self.decode("utf-8")
+    def get_int(self, key: str) -> int:
+        """doc doc method"""
+        return self.get(key, fn=int)
